31a0794281f1763df2519c47a1843d6806c2ec3d
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2023-03-07 16:27:16 -0600

    Add files via upload

---
+++ huffman/src/HuffmanTree.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+public class HuffmanTree {
+
+    public class Node implements Comparable<Integer>{
+        char c;
+
+
+        @Override
+        public int compareTo(Integer o) {
+
+            return 0;
+        }
+    }
+
+    PriorityQueue<Integer> tree;
+
+    public HuffmanTree(int[] counts){
+
+    }
+
+    public static void encode(BitOutputStream stream, String filename){
+
+    }
+}
