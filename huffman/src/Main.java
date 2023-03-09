2b81f76666b2dd9426e95f98969c1ba067fc71a1
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2023-03-09 12:26:21 -0600

    Add files via upload

---
+++ huffman/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //----------------------------------------------------------------------------
+        Scanner input = new Scanner(System.in);
+
+        String s = input.next();
+    
+        int[] count = toCharMap(s);
+
+        //----------------------------------------------------------------------------
+
+//         HuffmanTree tree = new HuffmanTree(count, s);
+//         tree.encode("out.txt");
+
+        HuffmanTree tree = new HuffmanTree("out.txt");
+
+        System.out.println("\n" + tree.encoded_string + "\n");
+        System.out.println(tree.decode(tree.tree, tree.encoded_string));
+
+        input.close();
+    }
+
+    public static int[] toCharMap(String s){
+        int[] count = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[((int)s.charAt(i))]++;
+        }
+
+        return count;
+    }
+}
